571ef592edf89b43da6acdcc63b2802ffcf884ba
Noh Byungkuk <raynear@gmail.com>
2021-08-26 15:44:15 +0900

    apply vp design

--- lib/view/vp.dart
+++ lib/view/vp.dart
@@ -72,137 +72,200 @@ class VP extends StatelessWidget {
           name == "제주패스" || name == "jejupass"
               ? InkWell(
                   onTap: () {
-                    Get.defaultDialog(
-                      backgroundColor: Colors.grey.shade200,
-                      title: "",
-                      titleStyle: TextStyle(fontSize: 0),
-                      // title: "인증 요청",
-                      // titleStyle: Get.textTheme.subtitle2!.copyWith(color: Colors.black, fontWeight: FontWeight.w700),
-                      content: Column(children: [
-                        Row(
-                          children: [
-                            Text('인증요청'),
-                            IconButton(
-                              icon: Icon(Icons.delete),
-                              onPressed: () {},
-                            )
-                          ],
-                        ),
-                        Text(thirdPartyName,
-                            style: Get.textTheme.headline6!.copyWith(color: Colors.black, fontWeight: FontWeight.w700)),
-                        SizedBox(height: 27),
-                        Text('제주패스 회원 10% 할인', style: Get.textTheme.subtitle1!.copyWith(color: Get.theme.accentColor)),
-                        SizedBox(height: 20),
-                        Text('$thirdPartyName에서 아래 정보를 요청합니다.', style: Get.textTheme.subtitle1),
-                        SizedBox(height: 12),
-                        Container(
-                          margin: EdgeInsets.only(bottom: 8),
-                          width: Get.width * 0.90,
-                          child: Card(
-                            color: Get.theme.cardColor,
-                            elevation: 1,
-                            shape: RoundedRectangleBorder(
-                              side: BorderSide(
-                                color: Colors.grey,
-                                width: 0.5,
+                    Get.dialog(
+                      Dialog(
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+                          insetPadding: EdgeInsets.all(30.0),
+                          child:
+                              // backgroundColor: Colors.grey.shade200,
+                              // title: "인증 요청",
+                              // titleStyle: Get.textTheme.subtitle2!.copyWith(color: Colors.black, fontWeight: FontWeight.w700),
+                              // content:
+                              Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Column(mainAxisSize: MainAxisSize.min, children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text('인증요청', style: Get.textTheme.subtitle2),
+                                  ),
+                                  IconButton(
+                                    icon: Icon(Icons.close),
+                                    onPressed: () {
+                                      Get.back();
+                                    },
+                                  )
+                                ],
                               ),
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            child: Padding(
-                                padding: EdgeInsets.all(15),
-                                child: Column(
-                                  children: [
-                                    Row(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        Padding(padding: EdgeInsets.all(10), child: Icon(FontAwesomeIcons.addressCard)),
-                                        Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                                          Text('  운전면허 정보', style: Get.textTheme.bodyText1),
-                                          Text('  성명, 생년월일, 운전면허번호', style: Get.textTheme.bodyText2)
-                                        ])
-                                      ],
+                              Text(thirdPartyName,
+                                  style: Get.textTheme.headline6!
+                                      .copyWith(color: Colors.black, fontWeight: FontWeight.w700)),
+                              SizedBox(height: 27),
+                              Text('제주패스 회원 10% 할인',
+                                  style: Get.textTheme.subtitle1!.copyWith(color: Get.theme.accentColor)),
+                              SizedBox(height: 20),
+                              Text('$thirdPartyName에서 아래 정보를 요청합니다.', style: Get.textTheme.subtitle1),
+                              SizedBox(height: 12),
+                              Container(
+                                margin: EdgeInsets.only(bottom: 8),
+                                width: Get.width * 0.90,
+                                child: Card(
+                                  color: Get.theme.cardColor,
+                                  elevation: 1,
+                                  shape: RoundedRectangleBorder(
+                                    side: BorderSide(
+                                      color: Colors.grey,
+                                      width: 0.5,
                                     ),
-                                  ],
-                                )),
-                          ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(bottom: 8),
-                          width: Get.width * 0.90,
-                          child: Card(
-                            color: Get.theme.cardColor,
-                            elevation: 1,
-                            shape: RoundedRectangleBorder(
-                              side: BorderSide(
-                                color: Colors.grey,
-                                width: 0.5,
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: Padding(
+                                      padding: EdgeInsets.all(15),
+                                      child: Column(
+                                        children: [
+                                          Row(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Padding(
+                                                  padding: EdgeInsets.all(10),
+                                                  child: Icon(FontAwesomeIcons.addressCard)),
+                                              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                                                Text('  운전면허 정보', style: Get.textTheme.bodyText1),
+                                                Text('  성명, 생년월일, 운전면허번호', style: Get.textTheme.bodyText2)
+                                              ])
+                                            ],
+                                          ),
+                                        ],
+                                      )),
+                                ),
                               ),
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            child: Padding(
-                                padding: EdgeInsets.all(15),
-                                child: Column(
-                                  children: [
-                                    Row(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        Padding(padding: EdgeInsets.all(10), child: Icon(FontAwesomeIcons.ticketAlt)),
-                                        Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                                          Text('  제주패스 정보', style: Get.textTheme.bodyText1),
-                                          Text('  성명, 예약번호, 유효기간', style: Get.textTheme.bodyText2)
-                                        ])
-                                      ],
+                              Container(
+                                margin: EdgeInsets.only(bottom: 8),
+                                width: Get.width * 0.90,
+                                child: Card(
+                                  color: Get.theme.cardColor,
+                                  elevation: 1,
+                                  shape: RoundedRectangleBorder(
+                                    side: BorderSide(
+                                      color: Colors.grey,
+                                      width: 0.5,
                                     ),
-                                  ],
-                                )),
-                          ),
-                        ),
-                      ]),
-                      confirm: ElevatedButton(
-                        child: Text('인증하기'),
-                        style: Get.theme.textButtonTheme.style,
-                        onPressed: () async {
-                          var vpResult = await c.testVP([vc]);
-                          print("#######################$vpResult########################");
-                          if ((vpResult / 100).floor() == 2) {
-                            await Get.defaultDialog(
-                                title: "인증 완료",
-                                content: Text("인증이 성공적으로 완료 되었습니다."),
-                                confirm: ElevatedButton(
-                                  child: Text('ok'),
-                                  onPressed: () {
-                                    Get.back();
-                                  },
-                                ));
-                          } else {
-                            await Get.defaultDialog(
-                                title: "VP 인증 실패",
-                                content: Text("인증이 실패 하였습니다."),
-                                confirm: ElevatedButton(
-                                  child: Text('ok'),
-                                  onPressed: () {
-                                    Get.back();
-                                  },
-                                ));
-                          }
-                          Get.back();
-                        },
-                      ),
-                      cancel: ElevatedButton(
-                        child: Text('취소'),
-                        style: Get.theme.textButtonTheme.style,
-                        onPressed: () {
-                          Get.back();
-                        },
-                      ),
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: Padding(
+                                      padding: EdgeInsets.all(15),
+                                      child: Column(
+                                        children: [
+                                          Row(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Padding(
+                                                  padding: EdgeInsets.all(10), child: Icon(FontAwesomeIcons.ticketAlt)),
+                                              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                                                Text('  제주패스 정보', style: Get.textTheme.bodyText1),
+                                                Text('  성명, 예약번호, 유효기간', style: Get.textTheme.bodyText2)
+                                              ])
+                                            ],
+                                          ),
+                                        ],
+                                      )),
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  TextButton(
+                                    child: Text('인증하기'),
+                                    style: Get.theme.textButtonTheme.style,
+                                    onPressed: () async {
+                                      var vpResult = await c.testVP([vc]);
+                                      print("#######################$vpResult########################");
+                                      if ((vpResult / 100).floor() == 2) {
+                                        await Get.defaultDialog(
+                                            title: "인증 완료",
+                                            content: Text("인증이 성공적으로 완료 되었습니다."),
+                                            confirm: ElevatedButton(
+                                              child: Text('ok'),
+                                              onPressed: () {
+                                                Get.back();
+                                              },
+                                            ));
+                                      } else {
+                                        await Get.defaultDialog(
+                                            title: "VP 인증 실패",
+                                            content: Text("인증이 실패 하였습니다."),
+                                            confirm: ElevatedButton(
+                                              child: Text('ok'),
+                                              onPressed: () {
+                                                Get.back();
+                                              },
+                                            ));
+                                      }
+                                      Get.back();
+                                    },
+                                  ),
+                                ],
+                              )
+                            ]),
+                          )),
+                      // confirm:
+                      // ElevatedButton(
+                      //   child: Text('인증하기'),
+                      //   style: Get.theme.textButtonTheme.style,
+                      //   onPressed: () async {
+                      //     var vpResult = await c.testVP([vc]);
+                      //     print("#######################$vpResult########################");
+                      //     if ((vpResult / 100).floor() == 2) {
+                      //       await Get.defaultDialog(
+                      //           title: "인증 완료",
+                      //           content: Text("인증이 성공적으로 완료 되었습니다."),
+                      //           confirm: ElevatedButton(
+                      //             child: Text('ok'),
+                      //             onPressed: () {
+                      //               Get.back();
+                      //             },
+                      //           ));
+                      //     } else {
+                      //       await Get.defaultDialog(
+                      //           title: "VP 인증 실패",
+                      //           content: Text("인증이 실패 하였습니다."),
+                      //           confirm: ElevatedButton(
+                      //             child: Text('ok'),
+                      //             onPressed: () {
+                      //               Get.back();
+                      //             },
+                      //           ));
+                      //     }
+                      //     Get.back();
+                      //   },
+                      // ),
+                      // cancel: ElevatedButton(
+                      //   child: Text('취소'),
+                      //   style: Get.theme.textButtonTheme.style,
+                      //   onPressed: () {
+                      //     Get.back();
+                      //   },
+                      // ),
                     );
                   },
                   child: Container(
                       margin: EdgeInsets.only(left: Get.width * 0.125, right: Get.width * 0.125, bottom: 20),
                       width: Get.width * 0.75,
                       child: Stack(children: [
-                        Image.asset('assets/images/car_box.png', width: 300),
-                        Image.asset('assets/images/sk_rentacar_logo.png', width: 50)
+                        Image.asset('assets/images/car_box.png', width: Get.width * 0.75),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.asset('assets/images/sk_rentacar_logo.png', width: 100),
+                        ),
+                        Positioned(
+                            left: 10,
+                            top: 70,
+                            child: Row(children: [
+                              Text('63,500원', style: Get.textTheme.headline5!.copyWith(fontWeight: FontWeight.w700)),
+                              Text('24시간', style: Get.textTheme.caption)
+                            ]))
                       ])),
                 )
               : SizedBox(height: 0, width: 0)
